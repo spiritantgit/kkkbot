35ae25cd7e76960d05b4d2ef0e3c4e31a4f82033
spiritantgit <127956257+spiritantgit@users.noreply.github.com>
2023-06-30 21:58:51 +0300

    Update kkkbot.go

--- cmd/kkkbot.go
+++ cmd/kkkbot.go
@@ -10,13 +10,64 @@ import (
 	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/hirosassa/zerodriver"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
 	telebot "gopkg.in/telebot.v3"
 )
 
 var (
 	// Telebot bot
-	TeleToken = os.Getenv("TELE_TOKEN")
+	TeleToken   = os.Getenv("TELE_TOKEN")
+	// MetricsHost exporter host:port
+	MetricsHost = os.Getenv("METRICS_HOST")
 )
+
+// Initialize OpenTelemetry
+func initMetrics(ctx context.Context) {
+
+	// Create a new OTLP Metric gRPC exporter with the specified endpoint and options
+	exporter, _ := otlpmetricgrpc.New(
+		ctx,
+		otlpmetricgrpc.WithEndpoint(MetricsHost),
+		otlpmetricgrpc.WithInsecure(),
+	)
+
+	// Define the resource with attributes that are common to all metrics.
+	// labels/tags/resources that are common to all metrics.
+	resource := resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String(fmt.Sprintf("kkkbot_%s", appVersion)),
+	)
+
+	// Create a new MeterProvider with the specified resource and reader
+	mp := sdkmetric.NewMeterProvider(
+		sdkmetric.WithResource(resource),
+		sdkmetric.WithReader(
+			// collects and exports metric data every 10 seconds.
+			sdkmetric.NewPeriodicReader(exporter, sdkmetric.WithInterval(10*time.Second)),
+		),
+	)
+
+	// Set the global MeterProvider to the newly created MeterProvider
+	otel.SetMeterProvider(mp)
+
+}
+
+func pmetrics(ctx context.Context, payload string) {
+	// Get the global MeterProvider and create a new Meter with the name "kkkbot_light_signal_counter"
+	meter := otel.GetMeterProvider().Meter("kkkbot_light_signal_counter")
+
+	// Get or create an Int64Counter instrument with the name "kkkbot_light_signal_<payload>"
+	counter, _ := meter.Int64Counter(fmt.Sprintf("kkkbot_light_signal_%s", payload))
+
+	// Add a value of 1 to the Int64Counter
+	counter.Add(ctx, 1)
+}
+
 
 // kkkbotCmd represents the kkkbot command
 var kkkbotCmd = &cobra.Command{
